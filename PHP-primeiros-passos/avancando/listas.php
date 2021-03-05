59665a81b999c99a82cb573b791806f8ab63c179
Elmer Eidam Junior <elmerejunior@gmail.com>
2021-03-04 21:04:34 -0300

    Arquivos do final do curso

--- PHP-primeiros-passos/avancando/listas.php
+++ PHP-primeiros-passos/avancando/listas.php
@@ -2,13 +2,22 @@
 
 $idadeList = [24, 15, 78, 36, 41, 29, 11];
 
+list($idadeElmer, $idadeJoao, $idadeMaria) = $idadeList;
+
 $umaIdade = $idadeList[5];
 
 echo $umaIdade . PHP_EOL;
 
 $idadeList[] = 20;
 
+echo PHP_EOL;
+
 foreach ($idadeList as $idade) {
     echo $idade . PHP_EOL;
 }
 
+echo PHP_EOL;
+
+echo $idadeElmer . PHP_EOL;
+echo $idadeJoao . PHP_EOL;
+echo $idadeMaria . PHP_EOL;
